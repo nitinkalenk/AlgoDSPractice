a14dfca53bc4ef6efd4f76f7f987b60d84420b56
nkale <nitinkale.nk@gmail.com>
2022-01-19 14:49:09 +0530

    count sorted vowel strings

---
+++ src/main/java/com/ds/dynamicprog/CountSortedVowelStrings.java
@@ -0,0 +1,19 @@
+package com.ds.dynamicprog;
+
+public class CountSortedVowelStrings {
+
+  // https://leetcode.com/problems/count-sorted-vowel-strings
+  public int countVowelStrings(int n) {
+    int a = 1, e = 1, i = 1, o = 1, u = 1;
+    while (n > 1) {
+      a = a + e + i + o + u;
+      e = e + i + o + u;
+      i = i + o + u;
+      o = o + u;
+      u = u;
+      n--;
+    }
+    return a + e + i + o + u;
+  }
+
+}
