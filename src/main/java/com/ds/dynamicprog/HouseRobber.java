f6764de16a84db5542b040f42d5b7fa7ab1f69ae
nkale <nitinkale.nk@gmail.com>
2022-01-20 13:32:14 +0530

    house robber problem

---
+++ src/main/java/com/ds/dynamicprog/HouseRobber.java
@@ -0,0 +1,27 @@
+package com.ds.dynamicprog;
+
+import java.util.Arrays;
+
+public class HouseRobber {
+
+  // https://leetcode.com/problems/house-robber/submissions/
+  private int rob(int houseNo, int[] moneyArr, int[] dp) {
+    if(houseNo < 0 || houseNo >= moneyArr.length) {
+      return 0;
+    }
+    if (dp[houseNo] != -1) {
+      return dp[houseNo];
+    }
+    int currentHouseRobbed = moneyArr[houseNo] + rob(houseNo + 2, moneyArr, dp);
+    int currentHouseNotRobbed = rob(houseNo + 1, moneyArr, dp);
+    dp[houseNo] = Math.max(currentHouseRobbed, currentHouseNotRobbed);
+    return dp[houseNo];
+  }
+
+  public int rob(int[] nums) {
+    int[] dp = new int[nums.length];
+    Arrays.fill(dp, -1);
+    return rob(0, nums, dp);
+  }
+
+}
